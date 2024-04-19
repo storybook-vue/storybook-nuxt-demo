bedb5b510d5a6ef7db261bd8e6ae28dd3323e298
chakAs3 <javachakir@gmail.com>
2024-04-19 09:45:21 +0400

    fix image url

--- components/MyNuxtImage.vue
+++ components/MyNuxtImage.vue
@@ -1,5 +1,21 @@
 <template>
-
-  <NuxtImg src="https://raw.githubusercontent.com/nuxt/nuxt/HEAD/.github/assets/banner.png" />
-
+  <div>
+    <NuxtImg
+      src="/images/sb-nuxt-logo.jpg"
+      :placeholder="[30, 20]"
+      width="300"
+      height="200"
+      class="rounded"
+    />
+    <br>
+    <h4>
+      responsive image with nuxt-image component:
+    </h4>
+    <NuxtPicture
+      src="/images/sb-nuxt-logo.jpg"
+      width="1204"
+      height="632"
+      sizes="xs:200px md:500px lg:1024"
+    />
+  </div>
 </template>
